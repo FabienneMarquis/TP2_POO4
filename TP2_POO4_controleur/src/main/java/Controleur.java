8b5e7f493b1b84e4c6981331e07b6962abd6dcc9
FabienneMarquis <fabienne.marquis@gmail.com>
2016-01-19 09:48:04 -0500

    blu blu blu

--- TP2_POO4_controleur/src/main/java/Controleur.java
+++ TP2_POO4_controleur/src/main/java/Controleur.java
@@ -1,5 +1,47 @@
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
 /**
- * Created by fabienne on 2016-01-12.
+ * Created by fabienne et gabriel  on 2016-01-12.
  */
-public class Controleur {
+public class Controleur extends Application implements Initializable{
+
+    @FXML
+    private GridPane root;
+
+    @FXML
+    private Scene scene;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        String path = "./main/resources/vue_tp2.fxml";
+        System.out.print(getClass().getResource(path));
+        root = FXMLLoader.load(getClass().getResource(
+                path));
+        scene = new Scene(root);
+
+
+        primaryStage.setTitle("TP2 - Threads et MVC");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
