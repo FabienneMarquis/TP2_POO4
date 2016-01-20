20975fa710c820b0f1b955490648852a4db5158b
FabienneMarquis <fabienne.marquis@gmail.com>
2016-01-20 14:15:23 -0500

    blu c'est plate

---
+++ TP2_POO4_modele/src/main/java/ThrConsumme.java
@@ -0,0 +1,19 @@
+package main.java;
+
+/**
+ * Created by Fabienne et Gabriel on 2016-01-20.
+ */
+public class ThrConsumme {
+    private int temps1;
+    private int deltaT1;
+
+
+    ThrConsumme (Reserve reserve, int temps1, int deltaT1){
+
+    }
+    void run( ){
+
+    }
+
+
+}
