70307510698f5b905f7fd70ccf34226b1f1c0d34
FabienneMarquis <fabienne.marquis@gmail.com>
2016-01-21 13:06:26 -0500

    fxml fonctionne, rester à travailler les threads pi le modèle et le controleur ... fack bcp de choses

--- TP2_POO4_modele/src/main/java/ThrProduction.java
+++ TP2_POO4_modele/src/main/java/ThrProduction.java
@@ -3,15 +3,20 @@ package main.java;
 /**
  * Created by Fabienne et Gabriel on 2016-01-20.
  */
-public class ThrProduction {
+public class ThrProduction extends Thread {
+
     private int temps2;
     private int deltaT2;
 
+
     ThrProduction(Reserve reserve, int temps2, int deltaT2){
+        this.deltaT2= deltaT2;
 
     }
     public void run(){
-
+        if(ThrProduction.this != null){
+            ThrProduction.this.run();
+        }
     }
 
 }
